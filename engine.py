f33d462ef0d6c656f7aec5595941577b61fc73ef
isLinXu <2267379130@qq.com>
2023-08-01 07:42:09 +0800

    update engine.py

--- engine.py
+++ engine.py
@@ -9,6 +9,8 @@ class Particle:
         self.vy = random.uniform(-1, 1)
         self.ax = 0
         self.ay = 0.1
+        self.color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+        self.size = random.randint(3, 8)
 
     def update(self):
         self.vx += self.ax
@@ -30,6 +32,18 @@ class Scene:
             if particle.y < 0 or particle.y > self.height:
                 particle.vy *= -1
 
+    def handle_mouse_motion(self, mouse_x, mouse_y):
+        for particle in self.particles:
+            dx = mouse_x - particle.x
+            dy = mouse_y - particle.y
+            distance = (dx**2 + dy**2)**0.5
+            if distance < 100:
+                particle.ax = -dx / distance * 0.5
+                particle.ay = -dy / distance * 0.5
+            else:
+                particle.ax = 0
+                particle.ay = 0.1
+
 class Renderer:
     def __init__(self, width, height):
         self.width = width
@@ -41,7 +55,7 @@ class Renderer:
     def render(self, scene):
         self.screen.fill((0, 0, 0))
         for particle in scene.particles:
-            pygame.draw.circle(self.screen, (255, 255, 255), (int(particle.x), int(particle.y)), 5)
+            pygame.draw.circle(self.screen, particle.color, (int(particle.x), int(particle.y)), particle.size)
 
         pygame.display.flip()
 
@@ -57,6 +71,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEMOTION:
+                mouse_x, mouse_y = event.pos
+                scene.handle_mouse_motion(mouse_x, mouse_y)
 
         scene.update()
         renderer.render(scene)
